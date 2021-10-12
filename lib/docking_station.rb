ba4f4f2934b12b5ffbef47d1e59b2a52fe023d7e
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-12 17:04:35 +0100

    Chapter 11 Added docking method and attribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,16 @@
 require_relative 'bike'
 class DockingStation
+   attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def return_bike(bike)
+    @bike = bike
+  end
+
+  #def bike
+    #return @bike
+  #end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,22 @@
 require "docking_station.rb"
 
 describe DockingStation do
-  new_instance = DockingStation.new
-
+  bike = DockingStation.new
   it 'DockingStation releases bike' do
-    expect(new_instance.respond_to?(:release_bike)).to eq(true)
+    expect(bike.respond_to?(:release_bike)).to eq(true)
   end 
 
   it 'Release a working bike' do
     station = DockingStation.new
     bike = station.release_bike
     expect(bike.working?).to eq(true)
+  end
+
+  it 'Returns a bike to the docking station' do
+    bike = Bike.new
+    subject.return_bike(bike)
+    expect(subject.bike).to eq bike
+    #station = DockingStation.new
+    #expect(station.return_bike(bike)).to eq(bike)
   end
 end
